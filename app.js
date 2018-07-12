cef36a442c3c1b87972bfd1a5f339fb6b25ded00
Danny Lu <dannylu8@outlook.com>
2018-07-12 16:51:17 -0700

    added error handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
                                               //no .js
@@ -6,9 +7,31 @@ const productRoutes = require('./api/routes/products');
 
 const orderRoutes = require('./api/routes/orders');
 
+
+app.use(morgan('dev'));
+
+
 //all the url ending with /products, will be forward to productRoutes, which is product.js
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//handle 404 error
+app.use('/', (request, response ,next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  //pass error, forward request in line 20
+  next(error);
+});
+
+//handle all different error
+app.use('/', (error,request, response, next) =>{
+  response.status(error.status || 500);
+  response.json({
+    error:{
+    	message:error.message
+    }
+  });
+});
 
 //don't need this anymore, we will use something else to set up the server
 
