c7d79cdfb90ed1e7059f701a78ac8dc29e682f39
Danny Lu <dannylu8@outlook.com>
2018-07-13 17:33:20 -0700

    added feature to filter the content we want using populate()

---
+++ api/models/order.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+  _id:mongoose.Schema.Types.ObjectId,
+             //type --> read unique id from above    //reference model call Product
+  product: {type: mongoose.Schema.Types.Object, ref:'Product', required:true},
+  quantity: {type: Number, default:1}
+});
+
+                     //this model is name schema, load schema name 'oderSchema'
+module.exports = mongoose.model('Order', orderSchema)
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,33 +1,102 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+const Order = require('../models/order.js');
+const Product = require('../models/product.js');
+
 router.get('/', (request, response, next) => {
-  response.status(200).json({
-    message:'message were fetched'
+  //dont need static
+  // response.status(200).json({
+  //   message:'message were fetched'
+  // });
+  //no arg means find all
+  Order.find()
+  //only response those 3 category
+  .select('product quantity _id')
+  //populate will show all information that in product.js too
+  .populate('product', 'name') //---> if added 2nd argument, only show name prop in product when call /order
+  .exec()
+  .then(docs =>{
+  	response.status(200).json(docs);
+  })
+  .catch(err =>{
+  	response.stats(500).json({
+  		error:err
+  	});
   });
 });
 
 router.post('/', (request,response,next) =>{
-	const order = {
-      productId: request.body.productId,
-      quantity:request.body.quantity
-	};
-  response.status(201).json({
-    message:'Order was created',
-    order:order
-  });
+	
+
+	//don not need these static file anymore
+	// const order = {
+ //      productId: request.body.productId,
+ //      quantity:request.body.quantity
+	// };
+
+	//using Order constructor to make new order, and we can configure it with object
+	const order = new Order({
+		                   //objectId is call to make new unique id
+		_id: mongoose.Types.ObjectId(),
+		quantity: request.body.quantity,
+		product: request.body.productId
+	});
+	order.save()
+    //exec is not needed here, exec turn into real promise, need that for findbyid,
+    //but not for save, save has buildin
+	//.exec()
+	.then(result =>{
+		console.log(result);
+		response.status(201).json(result);
+	})
+	.catch( err => {
+		response.status(500).json({
+			error:err
+		});
+	});
+ 
+  //don't need this status anymore
+  // response.status(201).json({
+  //   message:'Order was created',
+  //   order:order
+  // });
 });
 
 router.get('/:orderId', (request,response,next) =>{
-  response.status(200).json({
-    message:'Order details',
-    orderId:request.params.orderId
+  Order.findById(request.params.orderId)
+  .populate('product')
+  .exec()
+  .then(order =>{
+  	response.status(200).json({
+       order:order
+  	});
+  })
+  .catch(err =>{
+  	response.status(500).json({
+  		error:err
+  	});
   });
+  //static
+  // response.status(200).json({
+  //   message:'Order details',
+  //   orderId:request.params.orderId
+  // });
 });
 
 router.delete('/:orderId', (request, response, next) =>{
-	response.status(200).json({
-      message:'Order delete'
+	//remove by id
+	Order.remove({_id:request.params.orderId})
+	.exec()
+	.then( result =>{
+		message:'order deleted'
+	})
+	.catch( err=>{
+		response.status(500).json({
+			error:err
+		});
 	});
 })
 
