89c1a6bbb4871bcc997b0c96ac2158190a50bb0a
Danny Lu <dannylu8@outlook.com>
2018-07-16 12:15:04 -0700

    added user login, delete with bcrypt password protection

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
 	_id:mongoose.Schema.Types.ObjectId,
-	email:{type:String, required:true},
+	email:{
+		type:String, 
+		required:true, 
+		//these regExp will make sure input is an actual e-mail
+		match:/^\S+@\S+\.\S+$/
+	},
 	password: {type:String, required:true}
 });
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -10,11 +10,95 @@ const bcrypt = require('bcrypt');
 
 
 router.post('/signup', (request, response, next ) =>{
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email:request.body.email,
-    password:bcrypt.hash(request.body.password, )
-  });
+  //prevent signup with same email address
+  User.find({email: request.body.email})
+    .exec()
+    .then(user =>{
+    	//don't use if(user), if you do, user will be just an empty array, it still exist
+    	//so we should use user.length >0
+    	if(user.length > 0){             //409 = conflict, 422 is ok too
+    		return response.status(409).json({
+    			message: 'email already exist'
+    		})
+    	}else{
+    		bcrypt.hash(request.body.password, 10, (err,hash) =>{
+               if(err){
+      	         return response.status(500).json({
+      		     error:err
+      	       });
+             }else {
+      	
+               const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email:request.body.email,
+                password: hash
+              });
+                 user.save()
+                     .then(result =>{
+         	             console.log(result);
+         	             response.status(201).json({
+         	             message: 'User created'
+                         })
+                     })
+                    .catch(err =>{
+                   	console.log(err);
+         	        response.status(500).json({
+         		    error:err
+         	          });
+                    });
+                }
+             });
+    	   }
+        })
+ });
+
+router.post('/login', (request, response, next) =>{
+  User.find({email: request.body.email})
+      .exec()
+      .then(user =>{
+      	if (user.length < 1){    //unauthorized
+      		return response.status(401).json({
+      			message:'Authorization failed'
+      		})
+      	}
+      	bcrypt.compare(request.body.password, user[0].password, (err, result) =>{
+          if(err){
+          	return response.status(401).json({
+              message:'Authorization failed'
+          	});
+          }
+          if(result){
+          	 return response.status(200).json({
+          	 	message: 'Authorization successful'
+          	 });
+          }
+          response.status(401).json({
+            message:'Authorization failed'
+          });
+      	});
+      })
+      .catch( err=>{
+      	console.log(err);
+      	response.status(500).json({
+      		error:err
+      	})
+      });
+});
+
+router.delete('/:userId', (request, response, next) =>{
+  User.remove({_id: request.params.userId})
+    .exec()
+    .then( result =>{
+    	response.status(200).json({
+          message:'user deleted'
+    	});
+    })
+    .catch( err =>{
+    	console.log(err);
+    	response.status(500).json({
+          error:err
+    	});
+    });
 });
 
 module.exports = router;
